fe3c648837a7eac2621fdb4cf108230734838f60
Chris Moesel <cmoesel@users.noreply.github.com>
2019-11-06 13:24:12 -0500

    Update FHIR to 4.0.1 (#6)

--- test/fhirdefs/load.test.ts
+++ test/fhirdefs/load.test.ts
@@ -2,73 +2,95 @@ import { load } from '../../src/fhirdefs/load';
 import { FHIRDefinitions } from '../../src/fhirdefs/FHIRDefinitions';
 
 describe('#load()', () => {
-  describe('fhir-4.0.0', () => {
+  describe('fhir-4.0.1', () => {
     let defs: FHIRDefinitions;
     beforeAll(() => {
-      defs = load('4.0.0');
+      defs = load('4.0.1');
     });
 
     it('should load base FHIR resources', () => {
       expect(defs.allResources().some(r => r.id === 'Condition')).toBeTruthy();
-      expect(defs.findResource('Condition').url).toBe(
-        'http://hl7.org/fhir/StructureDefinition/Condition'
-      );
-      expect(defs.findResource('http://hl7.org/fhir/StructureDefinition/Condition').id).toBe(
-        'Condition'
+      const conditionByID = defs.findResource('Condition');
+      expect(conditionByID.url).toBe('http://hl7.org/fhir/StructureDefinition/Condition');
+      expect(conditionByID.fhirVersion).toBe('4.0.1');
+      expect(defs.findResource('http://hl7.org/fhir/StructureDefinition/Condition')).toEqual(
+        conditionByID
       );
     });
 
     it('should load base FHIR primitive types', () => {
       expect(defs.allTypes().some(r => r.id === 'boolean')).toBeTruthy();
-      expect(defs.findType('boolean').url).toBe('http://hl7.org/fhir/StructureDefinition/boolean');
-      expect(defs.findType('http://hl7.org/fhir/StructureDefinition/boolean').id).toBe('boolean');
+      const booleanByID = defs.findType('boolean');
+      expect(booleanByID.url).toBe('http://hl7.org/fhir/StructureDefinition/boolean');
+      expect(booleanByID.fhirVersion).toBe('4.0.1');
+      expect(defs.findType('http://hl7.org/fhir/StructureDefinition/boolean')).toEqual(booleanByID);
     });
 
     it('should load base FHIR complex types', () => {
       expect(defs.allTypes().some(r => r.id === 'Address')).toBeTruthy();
-      expect(defs.findType('Address').url).toBe('http://hl7.org/fhir/StructureDefinition/Address');
-      expect(defs.findType('http://hl7.org/fhir/StructureDefinition/Address').id).toBe('Address');
+      const addressByID = defs.findType('Address');
+      expect(addressByID.url).toBe('http://hl7.org/fhir/StructureDefinition/Address');
+      expect(addressByID.fhirVersion).toBe('4.0.1');
+      expect(defs.findType('http://hl7.org/fhir/StructureDefinition/Address')).toEqual(addressByID);
     });
 
     it('should load base FHIR extensions', () => {
       expect(defs.allExtensions().some(r => r.id === 'patient-mothersMaidenName')).toBeTruthy();
-      expect(defs.findExtension('patient-mothersMaidenName').url).toBe(
+      const maidenNameExtensionByID = defs.findExtension('patient-mothersMaidenName');
+      expect(maidenNameExtensionByID.url).toBe(
         'http://hl7.org/fhir/StructureDefinition/patient-mothersMaidenName'
       );
+      expect(maidenNameExtensionByID.fhirVersion).toBe('4.0.1');
       expect(
-        defs.findExtension('http://hl7.org/fhir/StructureDefinition/patient-mothersMaidenName').id
-      ).toBe('patient-mothersMaidenName');
+        defs.findExtension('http://hl7.org/fhir/StructureDefinition/patient-mothersMaidenName')
+      ).toEqual(maidenNameExtensionByID);
     });
 
     it('should load base FHIR value sets', () => {
       expect(defs.allValueSets().some(r => r.id === 'allergyintolerance-clinical')).toBeTruthy();
-      expect(defs.findValueSet('allergyintolerance-clinical').url).toBe(
+      const allergyStatusValueSetByID = defs.findValueSet('allergyintolerance-clinical');
+      expect(allergyStatusValueSetByID.url).toBe(
         'http://hl7.org/fhir/ValueSet/allergyintolerance-clinical'
       );
-      expect(defs.findValueSet('http://hl7.org/fhir/ValueSet/allergyintolerance-clinical').id).toBe(
-        'allergyintolerance-clinical'
+      // For some reason, value sets don't specify a fhirVersion, but in this case the business
+      // version is the FHIR version, so we'll verify that instead
+      expect(allergyStatusValueSetByID.version).toBe('4.0.1');
+      expect(defs.findValueSet('http://hl7.org/fhir/ValueSet/allergyintolerance-clinical')).toEqual(
+        allergyStatusValueSetByID
       );
     });
 
     it('should globally find any definition', () => {
-      expect(defs.find('Condition').kind).toBe('resource');
-      expect(defs.find('http://hl7.org/fhir/StructureDefinition/Condition').kind).toBe('resource');
-      expect(defs.find('boolean').kind).toBe('primitive-type');
-      expect(defs.find('http://hl7.org/fhir/StructureDefinition/boolean').kind).toBe(
-        'primitive-type'
-      );
-      expect(defs.find('Address').kind).toBe('complex-type');
-      expect(defs.find('http://hl7.org/fhir/StructureDefinition/Address').kind).toBe(
-        'complex-type'
-      );
-      expect(defs.find('patient-mothersMaidenName').type).toBe('Extension');
+      const conditionByID = defs.find('Condition');
+      expect(conditionByID.kind).toBe('resource');
+      expect(conditionByID.fhirVersion).toBe('4.0.1');
+      expect(defs.find('http://hl7.org/fhir/StructureDefinition/Condition')).toEqual(conditionByID);
+
+      const booleanByID = defs.find('boolean');
+      expect(booleanByID.kind).toBe('primitive-type');
+      expect(booleanByID.fhirVersion).toBe('4.0.1');
+      expect(defs.find('http://hl7.org/fhir/StructureDefinition/boolean')).toEqual(booleanByID);
+
+      const addressByID = defs.find('Address');
+      expect(addressByID.kind).toBe('complex-type');
+      expect(addressByID.fhirVersion).toBe('4.0.1');
+      expect(defs.find('http://hl7.org/fhir/StructureDefinition/Address')).toEqual(addressByID);
+
+      const maidenNameExtensionByID = defs.find('patient-mothersMaidenName');
+      expect(maidenNameExtensionByID.type).toBe('Extension');
+      expect(maidenNameExtensionByID.fhirVersion).toBe('4.0.1');
       expect(
-        defs.find('http://hl7.org/fhir/StructureDefinition/patient-mothersMaidenName').type
-      ).toBe('Extension');
-      expect(defs.find('allergyintolerance-clinical').resourceType).toBe('ValueSet');
-      expect(
-        defs.find('http://hl7.org/fhir/ValueSet/allergyintolerance-clinical').resourceType
-      ).toBe('ValueSet');
+        defs.find('http://hl7.org/fhir/StructureDefinition/patient-mothersMaidenName')
+      ).toEqual(maidenNameExtensionByID);
+
+      const allergyStatusValueSetByID = defs.find('allergyintolerance-clinical');
+      expect(allergyStatusValueSetByID.resourceType).toBe('ValueSet');
+      // For some reason, value sets don't specify a fhirVersion, but in this case the business
+      // version is the FHIR version, so we'll verify that instead
+      expect(allergyStatusValueSetByID.version).toBe('4.0.1');
+      expect(defs.find('http://hl7.org/fhir/ValueSet/allergyintolerance-clinical')).toEqual(
+        allergyStatusValueSetByID
+      );
     });
   });
 });
