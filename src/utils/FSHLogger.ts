67e63533e1ff1869df6e6568501ee91d3b4737db
Nicholas Freiter <nfreiter@mitre.org>
2020-02-17 14:38:07 -0500

    Update message coloring

--- src/utils/FSHLogger.ts
+++ src/utils/FSHLogger.ts
@@ -1,6 +1,7 @@
 import { createLogger, format, transports } from 'winston';
+import chalk from 'chalk';
 
-const { combine, colorize, simple } = format;
+const { combine, printf } = format;
 
 const withLocation = format(info => {
   if (info.file) {
@@ -38,8 +39,30 @@ const incrementCounts = format(info => {
   return info;
 });
 
+const printer = printf(info => {
+  let level;
+  switch (info.level) {
+    case 'info':
+      level = chalk.whiteBright.bgGreen(`${info.level} `);
+      break;
+    case 'warn':
+      // (255, 140, 0) = dark orange
+      level = chalk.whiteBright.bgRgb(255, 140, 0)(`${info.level} `);
+      break;
+    case 'error':
+      level = chalk.whiteBright.bgRed(`${info.level}`);
+      break;
+    case 'debug':
+      level = chalk.whiteBright.bgBlue(`${info.level}`);
+      break;
+    default:
+      break;
+  }
+  return `${level} ${info.message}`;
+});
+
 export const logger = createLogger({
-  format: combine(incrementCounts(), withLocation(), colorize({ all: true }), simple()),
+  format: combine(incrementCounts(), withLocation(), printer),
   transports: [new transports.Console()]
 });
 
