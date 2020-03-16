f4c4054475a844d06722996adb75fdf49db89ee0
Julia Afeltra <30803904+jafeltra@users.noreply.github.com>
2020-03-16 15:20:49 -0400

    Update IG scripts (#283)
    * Update updatePublisher and genonce scripts. Add gencontinuous script.
    
    * Minor readme and script updates based on review

--- src/ig/files/_genonce.sh
+++ src/ig/files/_genonce.sh
@@ -1,3 +1,29 @@
 #!/bin/bash
+publisher_jar=org.hl7.fhir.publisher.jar
+input_cache_path=./input-cache/
 set -e
-java -jar input-cache/org.hl7.fhir.publisher.jar -ig ig.ini
+echo Checking internet connection...
+curl -sSf tx.fhir.org > /dev/null
+
+if [ $? -eq 0 ]; then
+	echo "Online"
+	txoption=""
+else
+	echo "Offline"
+	txoption="-tx n/a"
+fi
+
+echo "$txoption"
+
+publisher=$input_cache_path/$publisher_jar
+if test -f "$publisher"; then
+	JAVA -jar $publisher -ig ig.ini $txoption $*
+
+else
+	publisher=../$publisher_jar
+	if test -f "$publisher"; then
+		JAVA -jar $publisher -ig ig.ini $txoption $*
+	else
+		echo IG Publisher NOT FOUND in input-cache or parent folder.  Please run _updatePublisher.  Aborting...
+	fi
+fi
