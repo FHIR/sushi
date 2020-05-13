da4a7fc24643aa4aa968db9bd456e358073b4c7d
Nicholas Freiter <nfreiter@mitre.org>
2020-05-13 12:30:52 -0400

    Fix failing tests after rebase

--- test/export/InstanceExporter.test.ts
+++ test/export/InstanceExporter.test.ts
@@ -2008,7 +2008,7 @@ describe('InstanceExporter', () => {
         expect(exported.contained).toEqual([
           {
             id: 'MyBundle',
-            meta: { profile: ['http://example.com/StructureDefinition/TestBundle'] },
+            meta: { profile: ['http://hl7.org/fhir/us/minimal/StructureDefinition/TestBundle'] },
             resourceType: 'Bundle',
             entry: [
               {
