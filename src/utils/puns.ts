1571caa4fa5d144cab6cc757ba439676e7dfd0d2
Chris Moesel <cmoesel@users.noreply.github.com>
2021-01-13 13:29:05 -0500

    Puns and punting prettiness (#725)
    * Export puns for re-use
    
    * Detect Java and avoid fancy characters
    
    The approach is not perfect, but hopefully catches most cases.
    
    * Remove unnecessary import

---
+++ src/utils/puns.ts
@@ -0,0 +1,70 @@
+import { sample } from 'lodash';
+
+const CLEAN_RUN_PUNS = [
+  'That went swimmingly!',
+  'O-fish-ally error free!',
+  "Nice! You're totally krilling it!",
+  'Cool and So-fish-ticated!',
+  'Well hooked and landed!',
+  'You earned a PhD in Ichthyology!',
+  'You rock, lobster!',
+  'Everything is ship-shape!',
+  'Ex-clam-ation point!',
+  'Ac-clam-ations!',
+  'Fin-tastic job!',
+  'You are dolphinitely doing great!',
+  'It doesn’t get any betta than this!',
+  'You’re piranha roll now!'
+];
+
+const WARNING_PUNS = [
+  'Not bad, but you cod do batter!',
+  'Something smells fishy...',
+  'Warnings... Water those about?',
+  'Looks like you are casting about.',
+  'A bit pitchy, but tuna-ble.',
+  'Do you sea the problem?',
+  'You are skating on fin ice.',
+  'You should mullet over.'
+];
+
+const ERROR_PUNS = [
+  'Ick! Errors!',
+  'Some-fin went wrong...',
+  'Unfor-tuna-tely, there are errors.',
+  'That really smelt.',
+  'You spawned some errors.',
+  'Just keep swimming, Dory.',
+  'This is the one that got away.',
+  'The docs might be bene-fish-al.',
+  'This was a turtle disaster.',
+  'Something went eely wrong there.',
+  'Documentation may be kelp-ful.',
+  'You should do some sole searching.',
+  'Call a FSH sturgeon!',
+  'This is giving me a haddock.',
+  'You whaley need to turn this around.'
+];
+
+export function getRandomPun(numErrors = 0, numWarnings = 0): string {
+  const puns = numErrors > 0 ? ERROR_PUNS : numWarnings > 0 ? WARNING_PUNS : CLEAN_RUN_PUNS;
+  return sample(puns);
+}
+
+export function getPuns(
+  includeErrors = true,
+  includeWarnings = true,
+  includeClean = true
+): string[] {
+  const puns: string[] = [];
+  if (includeErrors) {
+    puns.push(...ERROR_PUNS);
+  }
+  if (includeWarnings) {
+    puns.push(...WARNING_PUNS);
+  }
+  if (includeClean) {
+    puns.push(...CLEAN_RUN_PUNS);
+  }
+  return puns;
+}
