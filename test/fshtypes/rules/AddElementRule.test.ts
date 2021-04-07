ed341665a8d55603d8cf1f8e6f14fe54482e1e8b
Mint Thompson <mathompson@mitre.org>
2021-04-07 12:34:21 -0400

    AddElementRule adds toFSH method
    This method is used to produce FSH that, when processed, will create an
    instance of AddElementRule equivalent to the current instance. Several
    functions are added to a new file in the fshtypes directory, as they
    will be useful for implementing toFSH methods for other FSH classes.

--- test/fshtypes/rules/AddElementRule.test.ts
+++ test/fshtypes/rules/AddElementRule.test.ts
@@ -19,4 +19,96 @@ describe('AddElementRule', () => {
       expect(r.definition).toBeUndefined();
     });
   });
+
+  describe('#toFSH', () => {
+    it('should produce FSH for a rule with cardinality and one type', () => {
+      const rule = new AddElementRule('barley');
+      rule.min = 0;
+      rule.max = '*';
+      rule.types.push({
+        type: 'Quantity'
+      });
+      const expectedFSH = '* barley 0..* Quantity';
+      expect(rule.toFSH()).toEqual(expectedFSH);
+    });
+
+    it('should produce FSH for a rule that contains flags', () => {
+      const rule = new AddElementRule('road.brick');
+      rule.min = 0;
+      rule.max = '7';
+      rule.types.push({
+        type: 'Material',
+        isReference: true
+      });
+      rule.modifier = true;
+      rule.draft = true;
+      const expectedFSH = '* road.brick 0..7 ?! D Reference(Material)';
+      expect(rule.toFSH()).toEqual(expectedFSH);
+    });
+
+    it('should produce FSH for a rule that contains short text', () => {
+      const rule = new AddElementRule('barley');
+      rule.min = 0;
+      rule.max = '*';
+      rule.types.push({
+        type: 'Quantity'
+      });
+      rule.short = 'Fresh barley';
+      const expectedFSH = '* barley 0..* Quantity "Fresh barley"';
+      expect(rule.toFSH()).toEqual(expectedFSH);
+    });
+
+    it('should produce FSH for a rule that contains short and definition text', () => {
+      const rule = new AddElementRule('barley');
+      rule.min = 0;
+      rule.max = '*';
+      rule.types.push({
+        type: 'Quantity'
+      });
+      rule.short = 'Fresh barley';
+      rule.definition = 'Barley, one of the known ancient grains.';
+      const expectedFSH =
+        '* barley 0..* Quantity "Fresh barley" "Barley, one of the known ancient grains."';
+      expect(rule.toFSH()).toEqual(expectedFSH);
+    });
+
+    it('should produce FSH for a rule that contains multiple types', () => {
+      const rule = new AddElementRule('barley');
+      rule.min = 0;
+      rule.max = '*';
+      rule.types.push(
+        {
+          type: 'Quantity'
+        },
+        {
+          type: 'Specimen',
+          isReference: true
+        }
+      );
+      const expectedFSH = '* barley 0..* Quantity or Reference(Specimen)';
+      expect(rule.toFSH()).toEqual(expectedFSH);
+    });
+
+    it('should produce FSH for a rule that contains flags, multiple types, short text, and definition text', () => {
+      const rule = new AddElementRule('barley');
+      rule.min = 0;
+      rule.max = '*';
+      rule.types.push(
+        {
+          type: 'Quantity'
+        },
+        {
+          type: 'Specimen',
+          isReference: true
+        }
+      );
+      rule.trialUse = true;
+      rule.mustSupport = true;
+      rule.short = 'Fresh barley';
+      rule.definition = 'Barley, one of the "ancient grains."';
+      const expectedFSH =
+        '* barley 0..* MS TU Quantity or Reference(Specimen) "Fresh barley" "Barley, one of the \\"ancient grains.\\""';
+      expect(rule.toFSH()).toEqual(expectedFSH);
+    });
+  });
 });
