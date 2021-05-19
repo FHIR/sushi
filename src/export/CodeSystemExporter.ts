a58223af079cdbf3eb20aa5f01b3fcee766b10d4
Mint Thompson <mathompson@mitre.org>
2021-05-19 17:57:28 -0400

    Export CodeSystems with hierarchical codes (#814)
    * Export CodeSystems with hierarchical codes
    
    Concepts within a code system are arranged hierarchically based on the
    ConceptRule's specified hierarchy. The path to a hierarchical code must
    exist in order to be added, and this is checked when adding the
    ConceptRule to the FshCodeSystem.
    Additionally, a CodeCaretValueRule may be used to specify values on a
    concept anywhere in the hierarchy. The codePath on this rule is
    traversed after all the concepts are added so that the correct element
    in the CodeSystem can be located. Just like for CaretValueRules, the
    value to set is validated before being assigned.
    
    * Check for invalid hierarchy in export
    
    When exporting a code system, check if a concept's hierarchy actually
    exists. If it does not, show an error, and do not add the concept to the
    code system. Concepts added normally through FSH will always have valid
    hierarchies, but it is possible to add rules directly to a FshCodeSystem
    without using the addConcept method.
    
    CodeCaretValueRules don't support soft indexing, so there is no need to
    resolve soft indices on those rules.

--- src/export/CodeSystemExporter.ts
+++ src/export/CodeSystemExporter.ts
@@ -2,10 +2,11 @@ import { FSHTank } from '../import/FSHTank';
 import { CodeSystem, CodeSystemConcept, StructureDefinition } from '../fhirtypes';
 import { setPropertyOnInstance, applyInsertRules } from '../fhirtypes/common';
 import { FshCodeSystem } from '../fshtypes';
-import { CaretValueRule, ConceptRule } from '../fshtypes/rules';
+import { CaretValueRule, ConceptRule, CodeCaretValueRule } from '../fshtypes/rules';
 import { logger } from '../utils/FSHLogger';
 import { MasterFisher, Type, resolveSoftIndexing } from '../utils';
 import { Package } from '.';
+import { CannotResolvePathError } from '../errors';
 
 export class CodeSystemExporter {
   constructor(
@@ -26,19 +27,43 @@ export class CodeSystemExporter {
 
   private setConcepts(codeSystem: CodeSystem, concepts: ConceptRule[]): void {
     if (concepts.length > 0) {
-      codeSystem.concept = concepts.map(concept => {
-        const codeSystemConcept: CodeSystemConcept = { code: concept.code };
-        if (concept.display) codeSystemConcept.display = concept.display;
-        if (concept.definition) codeSystemConcept.definition = concept.definition;
-        return codeSystemConcept;
+      codeSystem.concept = [];
+      concepts.forEach(concept => {
+        let conceptContainer = codeSystem.concept;
+        const newConcept: CodeSystemConcept = { code: concept.code };
+        if (concept.display) {
+          newConcept.display = concept.display;
+        }
+        if (concept.definition) {
+          newConcept.definition = concept.definition;
+        }
+        for (const ancestorCode of concept.hierarchy) {
+          const ancestorConcept = conceptContainer.find(
+            ancestorConcept => ancestorConcept.code === ancestorCode
+          );
+          if (ancestorConcept) {
+            if (!ancestorConcept.concept) {
+              ancestorConcept.concept = [];
+            }
+            conceptContainer = ancestorConcept.concept;
+          } else {
+            logger.error(
+              `Could not find ${ancestorCode} in concept hierarchy to use as ancestor of ${concept.code}.`,
+              concept.sourceInfo
+            );
+            return;
+          }
+        }
+        conceptContainer.push(newConcept);
       });
     }
   }
 
-  private setCaretRules(codeSystem: CodeSystem, rules: CaretValueRule[]): void {
-    const csStructureDefinition = StructureDefinition.fromJSON(
-      this.fisher.fishForFHIR('CodeSystem', Type.Resource)
-    );
+  private setCaretRules(
+    codeSystem: CodeSystem,
+    csStructureDefinition: StructureDefinition,
+    rules: CaretValueRule[]
+  ): void {
     resolveSoftIndexing(rules);
     for (const rule of rules) {
       try {
@@ -54,10 +79,57 @@ export class CodeSystemExporter {
     }
   }
 
+  private setCodeCaretRules(
+    codeSystem: CodeSystem,
+    csStructureDefinition: StructureDefinition,
+    rules: CodeCaretValueRule[]
+  ) {
+    for (const rule of rules) {
+      try {
+        const conceptPath = this.findConceptPath(codeSystem, rule.codePath);
+        const { assignedValue, pathParts } = csStructureDefinition.validateValueAtPath(
+          `${conceptPath}.${rule.caretPath}`,
+          rule.value,
+          this.fisher
+        );
+        setPropertyOnInstance(codeSystem, pathParts, assignedValue, this.fisher);
+      } catch (e) {
+        logger.error(e.message, rule.sourceInfo);
+      }
+    }
+  }
+
+  private findConceptPath(codeSystem: CodeSystem, codePath: string[]): string {
+    const conceptIndices: number[] = [];
+    let conceptList = codeSystem.concept ?? [];
+    for (const codeStep of codePath) {
+      const stepIndex = conceptList.findIndex(concept => concept.code === codeStep);
+      if (stepIndex === -1) {
+        throw new CannotResolvePathError(codePath.map(code => `#${code}`).join(' '));
+      }
+      conceptIndices.push(stepIndex);
+      conceptList = conceptList[stepIndex].concept ?? [];
+    }
+    return conceptIndices.map(conceptIndex => `concept[${conceptIndex}]`).join('.');
+  }
+
+  private countConcepts(concepts: CodeSystemConcept[]): number {
+    if (concepts) {
+      return (
+        concepts.length +
+        concepts
+          .map(concept => this.countConcepts(concept.concept))
+          .reduce((sum, next) => sum + next, 0)
+      );
+    } else {
+      return 0;
+    }
+  }
+
   private updateCount(codeSystem: CodeSystem, fshDefinition: FshCodeSystem): void {
     // We can only derive a true count if the content is #complete
     if (codeSystem.content === 'complete') {
-      const actualCount = codeSystem.concept?.length;
+      const actualCount = this.countConcepts(codeSystem.concept) || undefined;
       if (codeSystem.count == null && actualCount != null) {
         codeSystem.count = actualCount;
       } else if (codeSystem.count !== actualCount) {
@@ -85,13 +157,22 @@ export class CodeSystemExporter {
     this.setMetadata(codeSystem, fshDefinition);
     // fshDefinition.rules may include insert rules, which must be expanded before applying other rules
     applyInsertRules(fshDefinition, this.tank);
+    const csStructureDefinition = StructureDefinition.fromJSON(
+      this.fisher.fishForFHIR('CodeSystem', Type.Resource)
+    );
     this.setCaretRules(
       codeSystem,
+      csStructureDefinition,
       fshDefinition.rules.filter(rule => rule instanceof CaretValueRule) as CaretValueRule[]
     );
     this.setConcepts(
       codeSystem,
       fshDefinition.rules.filter(rule => rule instanceof ConceptRule) as ConceptRule[]
+    );
+    this.setCodeCaretRules(
+      codeSystem,
+      csStructureDefinition,
+      fshDefinition.rules.filter(rule => rule instanceof CodeCaretValueRule) as CodeCaretValueRule[]
     );
 
     // check for another code system with the same id
