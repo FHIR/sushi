1ee8eccd3f8cdda22983a9997c8d539729aa928e
Julia Afeltra <jafeltra@mitre.org>
2021-07-19 13:12:06 -0400

    Parse codes based on the final # in a code to support fragements in systems

--- src/import/parseCodeLexeme.ts
+++ src/import/parseCodeLexeme.ts
@@ -9,7 +9,10 @@ import { FshCode } from '../fshtypes';
  * @returns {FshCode} the parse FshCode
  */
 export function parseCodeLexeme(conceptText: string): FshCode {
-  const splitPoint = conceptText.match(/(^|[^\\])(\\\\)*#/);
+  const allSplitPoints = [...conceptText.matchAll(/(^|[^\\])(\\\\)*#/g)];
+  const allSplitPointsArray = allSplitPoints.map(p => p);
+  let splitIndex = allSplitPointsArray.length - 1;
+  let splitPoint = allSplitPointsArray[splitIndex];
   let system: string, code: string;
   if (splitPoint == null) {
     system = '';
@@ -18,13 +21,22 @@ export function parseCodeLexeme(conceptText: string): FshCode {
     system = conceptText.slice(0, splitPoint.index) + splitPoint[0].slice(0, -1);
     code = conceptText.slice(splitPoint.index + splitPoint[0].length);
   }
-  system = system.replace(/\\\\/g, '\\').replace(/\\#/g, '#');
+  if (code.endsWith('"') && !code.startsWith('"')) {
+    // We matched on a # that is within a "quoted" code and we need to get an earlier #
+    while (!code.startsWith('"') && splitIndex >= 0) {
+      splitIndex = splitIndex - 1;
+      splitPoint = allSplitPointsArray[splitIndex];
+      system = conceptText.slice(0, splitPoint.index) + splitPoint[0].slice(0, -1);
+      code = conceptText.slice(splitPoint.index + splitPoint[0].length);
+    }
+  }
   if (code.startsWith('"')) {
     code = code
       .slice(1, code.length - 1)
       .replace(/\\\\/g, '\\')
       .replace(/\\"/g, '"');
   }
+  system = system.replace(/\\\\/g, '\\').replace(/\\#/g, '#');
   const concept = new FshCode(code);
   if (system.length > 0) {
     concept.system = system;
