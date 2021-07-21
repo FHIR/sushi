c2b7e1413b6cfc3b0096eb0a61dfdfc6f670e421
ngfreiter <nfreiter@mitre.org>
2021-07-21 10:33:01 -0400

    Remove unnecessary ? operator and add comment

--- src/import/FSHImporter.ts
+++ src/import/FSHImporter.ts
@@ -178,8 +178,10 @@ export class FSHImporter extends FSHVisitor {
       // Collect the aliases and store in global map
       ctx.entity().forEach(e => {
         if (e.alias()) {
-          const name = e.alias().SEQUENCE()[0]?.getText();
+          const name = e.alias().SEQUENCE()[0].getText();
           let value = e.alias().SEQUENCE()[1]?.getText();
+          // When the url contains a fragment (http://example.org#fragment), the grammar will read it as a
+          // CODE, so we also accept that for the value here
           if (!value && e.alias().CODE()) {
             value = e.alias().CODE().getText();
           }
