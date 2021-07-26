c82a8f96859ec96580534eb9fbeeca2716cd4233
julianxcarter <juliancarter55@gmail.com>
2021-07-26 09:28:48 -0400

    Windows fix

--- test/utils/leftAlign.ts
+++ test/utils/leftAlign.ts
@@ -1,11 +1,10 @@
-import { EOL } from 'os';
 /**
  * Removes white space in each non-empty line of a string, such that the first line is left-aligned
  * @param {string} inputFSH - The string to be made left-aligned
- * @returns {string} - The altered string to be left-aligned
+ * @returns {string} - The altered string
  */
 export function leftAlign(inputFSH: string): string {
-  const lineArray = inputFSH.split(EOL);
+  const lineArray = inputFSH.split(/\r?\n/);
   let offsetAmount: number;
   const letterRegex = /[a-z]/i;
 
@@ -16,5 +15,5 @@ export function leftAlign(inputFSH: string): string {
     }
   }
 
-  return lineArray.map(l => (l.length >= offsetAmount ? l.slice(offsetAmount) : l)).join(EOL);
+  return lineArray.map(l => (l.length >= offsetAmount ? l.slice(offsetAmount) : l)).join('\n');
 }
