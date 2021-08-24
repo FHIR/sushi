dc886627d58c3ed3837a1be9008a3096ae6a9188
Nick Freiter <52747882+ngfreiter@users.noreply.github.com>
2021-08-24 10:41:50 -0400

    Fix bug with indentation + soft indexing (#907)
    * Remove unneeded path handling
    
    * Remove unnecessary comment and add more explicit test

--- test/import/FSHImporter.context.test.ts
+++ test/import/FSHImporter.context.test.ts
@@ -189,6 +189,27 @@ describe('FSHImporter', () => {
       assertAssignmentRule(instance.rules[1], 'item[=].item[+].linkId', 'bar');
     });
 
+    it('should change + to = when setting context on children of soft indexed rules which are not path rules', () => {
+      const input = leftAlign(`
+      Instance: Foo
+      InstanceOf: Questionnaire
+      * item[+]
+        * linkId = "foo"
+        * item[+].linkId = "bar"
+    `);
+
+      const result = importSingleText(input, 'Context.fsh');
+      expect(loggerSpy.getAllMessages('error')).toHaveLength(0);
+      expect(loggerSpy.getAllMessages('warn')).toHaveLength(0);
+      expect(result.instances.size).toBe(1);
+      const instance = result.instances.get('Foo');
+      expect(instance.name).toBe('Foo');
+      expect(instance.instanceOf).toBe('Questionnaire');
+      expect(instance.rules.length).toBe(2);
+      assertAssignmentRule(instance.rules[0], 'item[+].linkId', 'foo');
+      assertAssignmentRule(instance.rules[1], 'item[=].item[+].linkId', 'bar');
+    });
+
     it('should change nested + to = when setting context on children of soft indexed rules', () => {
       const input = leftAlign(`
       Instance: Foo
