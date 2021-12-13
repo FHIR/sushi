d1a06b97cbb902bd1a107a80119e6296487182e7
Chris Moesel <cmoesel@mitre.org>
2021-12-13 11:31:18 -0500

    Convert dependency package ids to lowercase when extracted from IG resource
    A previous code change converted dependency package ids to lowercase when extracted from sushi-config.yaml.  This does the same for dependencies when extracted from an IG resource.

--- test/import/loadConfigurationFromIgResource.test.ts
+++ test/import/loadConfigurationFromIgResource.test.ts
@@ -46,16 +46,50 @@ describe('loadConfigurationFromIgResource', () => {
     expect(loggerSpy.getFirstMessage('info')).toMatch(
       new RegExp(`from ${escapeRegExp(path.join(inputPath, 'input', 'ImplementationGuide.json'))}`)
     );
-    expect(loggerSpy.getMessageAtIndex(1, 'info')).toEqual('  canonical: "http://example.org"');
-    expect(loggerSpy.getMessageAtIndex(2, 'info')).toEqual('  version: "1.0.0"');
-    expect(loggerSpy.getMessageAtIndex(3, 'info')).toEqual('  fhirVersion[0]: "4.0.1"');
-    expect(loggerSpy.getMessageAtIndex(4, 'info')).toEqual(
+    expect(loggerSpy.getMessageAtIndex(1, 'info')).toEqual('Extracted configuration:');
+    expect(loggerSpy.getMessageAtIndex(2, 'info')).toEqual('  canonical: "http://example.org"');
+    expect(loggerSpy.getMessageAtIndex(3, 'info')).toEqual('  version: "1.0.0"');
+    expect(loggerSpy.getMessageAtIndex(4, 'info')).toEqual('  fhirVersion[0]: "4.0.1"');
+    expect(loggerSpy.getMessageAtIndex(5, 'info')).toEqual(
       '  dependencies[0]: {"packageId":"foo.bar","version":"1.2.3"}'
     );
-    expect(loggerSpy.getMessageAtIndex(5, 'info')).toEqual(
+    expect(loggerSpy.getMessageAtIndex(6, 'info')).toEqual(
       '  dependencies[1]: {"packageId":"bar.foo","version":"current"}'
     );
-    expect(loggerSpy.getMessageAtIndex(6, 'info')).toEqual('  FSHOnly: true');
+    expect(loggerSpy.getMessageAtIndex(7, 'info')).toEqual('  FSHOnly: true');
+  });
+
+  it('should convert uppercase package Ids to lowercase', () => {
+    const inputPath = path.join(__dirname, 'fixtures', 'ig-JSON-uppercase-deps');
+    const config = loadConfigurationFromIgResource(inputPath);
+    expect(config).toEqual({
+      canonical: 'http://example.org',
+      FSHOnly: true,
+      dependencies: [
+        { packageId: 'foo.bar', version: '1.2.3' },
+        { packageId: 'bar.foo', version: 'current' },
+        { packageId: 'boo.far', version: '0.0.1' }
+      ],
+      fhirVersion: ['4.0.1'],
+      version: '1.0.0'
+    });
+    expect(loggerSpy.getFirstMessage('info')).toMatch(
+      new RegExp(`from ${escapeRegExp(path.join(inputPath, 'input', 'ImplementationGuide.json'))}`)
+    );
+    expect(loggerSpy.getMessageAtIndex(1, 'info')).toEqual('Extracted configuration:');
+    expect(loggerSpy.getMessageAtIndex(2, 'info')).toEqual('  canonical: "http://example.org"');
+    expect(loggerSpy.getMessageAtIndex(3, 'info')).toEqual('  version: "1.0.0"');
+    expect(loggerSpy.getMessageAtIndex(4, 'info')).toEqual('  fhirVersion[0]: "4.0.1"');
+    expect(loggerSpy.getMessageAtIndex(5, 'info')).toEqual(
+      '  dependencies[0]: {"packageId":"foo.bar","version":"1.2.3"}'
+    );
+    expect(loggerSpy.getMessageAtIndex(6, 'info')).toEqual(
+      '  dependencies[1]: {"packageId":"bar.foo","version":"current"}'
+    );
+    expect(loggerSpy.getMessageAtIndex(7, 'info')).toEqual(
+      '  dependencies[2]: {"packageId":"boo.far","version":"0.0.1"}'
+    );
+    expect(loggerSpy.getMessageAtIndex(8, 'info')).toEqual('  FSHOnly: true');
   });
 
   it('should extract an XML configuration with a url and dependencies', () => {
@@ -73,6 +107,49 @@ describe('loadConfigurationFromIgResource', () => {
     expect(loggerSpy.getFirstMessage('info')).toMatch(
       new RegExp(`from ${escapeRegExp(path.join(inputPath, 'input', 'ImplementationGuide.xml'))}`)
     );
+    expect(loggerSpy.getMessageAtIndex(1, 'info')).toEqual('Extracted configuration:');
+    expect(loggerSpy.getMessageAtIndex(2, 'info')).toEqual('  canonical: "http://example.org"');
+    expect(loggerSpy.getMessageAtIndex(3, 'info')).toEqual('  version: undefined');
+    expect(loggerSpy.getMessageAtIndex(4, 'info')).toEqual('  fhirVersion[0]: "4.0.1"');
+    expect(loggerSpy.getMessageAtIndex(5, 'info')).toEqual(
+      '  dependencies[0]: {"packageId":"foo.bar","version":"1.2.3"}'
+    );
+    expect(loggerSpy.getMessageAtIndex(6, 'info')).toEqual(
+      '  dependencies[1]: {"packageId":"bar.foo","version":"current"}'
+    );
+    expect(loggerSpy.getMessageAtIndex(7, 'info')).toEqual('  FSHOnly: true');
+  });
+
+  it('should convert uppercase package Ids to lowercase when extracting from XML configuration', () => {
+    const inputPath = path.join(__dirname, 'fixtures', 'ig-XML-uppercase-deps');
+    const config = loadConfigurationFromIgResource(inputPath);
+    expect(config).toEqual({
+      canonical: 'http://example.org',
+      FSHOnly: true,
+      dependencies: [
+        { packageId: 'foo.bar', version: '1.2.3' },
+        { packageId: 'bar.foo', version: 'current' },
+        { packageId: 'boo.far', version: '0.0.1' }
+      ],
+      fhirVersion: ['4.0.1']
+    });
+    expect(loggerSpy.getFirstMessage('info')).toMatch(
+      new RegExp(`from ${escapeRegExp(path.join(inputPath, 'input', 'ImplementationGuide.xml'))}`)
+    );
+    expect(loggerSpy.getMessageAtIndex(1, 'info')).toEqual('Extracted configuration:');
+    expect(loggerSpy.getMessageAtIndex(2, 'info')).toEqual('  canonical: "http://example.org"');
+    expect(loggerSpy.getMessageAtIndex(3, 'info')).toEqual('  version: undefined');
+    expect(loggerSpy.getMessageAtIndex(4, 'info')).toEqual('  fhirVersion[0]: "4.0.1"');
+    expect(loggerSpy.getMessageAtIndex(5, 'info')).toEqual(
+      '  dependencies[0]: {"packageId":"foo.bar","version":"1.2.3"}'
+    );
+    expect(loggerSpy.getMessageAtIndex(6, 'info')).toEqual(
+      '  dependencies[1]: {"packageId":"bar.foo","version":"current"}'
+    );
+    expect(loggerSpy.getMessageAtIndex(7, 'info')).toEqual(
+      '  dependencies[2]: {"packageId":"boo.far","version":"0.0.1"}'
+    );
+    expect(loggerSpy.getMessageAtIndex(8, 'info')).toEqual('  FSHOnly: true');
   });
 
   it('should find the ImplementationGuide JSON file even when other files are present', () => {
