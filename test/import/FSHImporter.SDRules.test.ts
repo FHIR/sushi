7d6c098e0d9d0c1a5a45c4093d60edcf674df3b5
Julia Afeltra <jafeltra@mitre.org>
2023-06-21 17:12:10 -0400

    Don't duplicate tests between FSHImporter.Profile and FSHImporter.SDRules

--- test/import/FSHImporter.SDRules.test.ts
+++ test/import/FSHImporter.SDRules.test.ts
@@ -1357,7 +1357,7 @@ describe('FSHImporter', () => {
         const input = leftAlign(`
         Profile: ObservationProfile
         Parent: Observation
-        * performer only CodeableReference(   Organization    or  CareTeam)
+        * performer only CodeableReference(   Organization    or  CareTeam      )
         `);
 
         const result = importSingleText(input);
