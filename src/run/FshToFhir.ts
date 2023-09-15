b51447b170bace60fe1962731914c3aa5a8bc5f1
Daniel Dubrovski <daniel@kodality.com>
2023-09-15 13:36:36 +0300

    convert logicals also

--- src/run/FshToFhir.ts
+++ src/run/FshToFhir.ts
@@ -75,7 +75,7 @@ export async function fshToFhir(
   // process FSH text into FHIR
   const outPackage = exportFHIR(tank, defs);
   const fhir: any[] = [];
-  (['profiles', 'extensions', 'instances', 'valueSets', 'codeSystems'] as const).forEach(
+  (['profiles', 'extensions', 'instances', 'valueSets', 'codeSystems', 'logicals'] as const).forEach(
     artifactType => {
       outPackage[artifactType].forEach((artifact: { toJSON: (snapshot: boolean) => any }) => {
         fhir.push(artifact.toJSON(false));
