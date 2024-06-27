7247b48a9a04f4ea7c4d051f06f35512a9bf1b9f
kjefferson <kjefferson@mitre.org>
2024-06-27 11:37:26 -0400

    combining conditional statements

--- src/fhirtypes/common.ts
+++ src/fhirtypes/common.ts
@@ -981,13 +981,11 @@ export function replaceReferences<T extends AssignmentRule | CaretValueRule>(
         clone = replaceReferences(clone, tank, fisher);
       } else if (type == null && id == null && value.reference) {
         // if the reference to an entity is provided but is unable to be resolved
-        if (!value.reference.startsWith('urn:')) {
-          if (!value.reference.includes('/')) {
-            logger.warn(
-              `Cannot find the entity referenced at ${value.reference}. The provided reference value will be used, but this reference does not conform to the FHIR Reference format.`,
-              rule.sourceInfo
-            );
-          }
+        if (!(value.reference.startsWith('urn:') || value.reference.includes('/'))) {
+          logger.warn(
+            `Cannot find the entity referenced at ${value.reference}. The provided reference value will be used, but this reference does not conform to the FHIR Reference format.`,
+            rule.sourceInfo
+          );
         }
       }
     }
