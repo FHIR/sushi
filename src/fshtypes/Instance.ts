55ef7c3158bc3257cbeb0103de40799ae990c935
Mint Thompson <mathompson@mitre.org>
2024-10-11 10:13:13 -0400

    Only return FshType id from rule value if the value is a string (#1524)
    When getting the id for a FshType, rules are checked to see if there is
    one that will set the id. A value that is not a string will not be valid
    and not be applied. Therefore, only return the value if it is a string.
    
    Rename test file for FshCodeSystem to match name of source file.

--- src/fshtypes/Instance.ts
+++ src/fshtypes/Instance.ts
@@ -25,8 +25,8 @@ export class Instance extends FshEntity {
 
   get id() {
     const assignedId = getNonInstanceValueFromRules(this, 'id', '', 'id');
-    if (assignedId) {
-      return assignedId.toString();
+    if (typeof assignedId === 'string') {
+      return assignedId;
     }
     return this._id;
   }
