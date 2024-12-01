23a9e2fcd3278ac199f5cb33ae2043c1cbd70a29
kjefferson <kjefferson@mitre.org>
2024-12-01 16:04:24 -0500

    missing prettier fix

--- src/fhirtypes/StructureDefinition.ts
+++ src/fhirtypes/StructureDefinition.ts
@@ -691,9 +691,7 @@ export class StructureDefinition {
       ) {
         // We throw an error if the currentElement doesn't exist, has been zeroed out,
         // or is being incorrectly accessed as an array
-        if (
-          currentPath == 'compose.include.concept[undefined]'
-        ) {
+        if (currentPath == 'compose.include.concept[undefined]') {
           throw new Error(
             'This rule is invalid. There is no concept array in the compose element with the code system: code_system.'
           );
